edbd0200c24e7e71397243a76d5415cbff45ff66
Chris Sheehan <chris.sheehan@ziglu.io>
2022-11-08 09:46:50 +0000

    argo workflows setup

--- setup.sh
+++ setup.sh
@@ -1,3 +1,23 @@
+# https://argoproj.github.io/argo-workflows/quick-start/
+
 minikube start
+
 kubectl create namespace argo
-kubectl apply -n argo -f https://raw.githubusercontent.com/argoproj/argo-workflows/master/manifests/quick-start-minimal.yaml
+# Get latest argo release from https://github.com/argoproj/argo-workflows/releases
+kubectl apply -n argo -f https://github.com/argoproj/argo-workflows/releases/download/v3.4.3/install.yaml
+while [[ $(kubectl get pods -n argo --no-headers -o jsonpath='{.items[0].status.phase}') != "Running" ]]; do
+   echo 'waiting for argo pods...'
+   sleep 3
+done
+
+kubectl patch deployment \
+  argo-server \
+  --namespace argo \
+  --type='json' \
+  -p='[{"op": "replace", "path": "/spec/template/spec/containers/0/args", "value": [
+  "server",
+  "--auth-mode=server"
+]}]'
+
+
+
